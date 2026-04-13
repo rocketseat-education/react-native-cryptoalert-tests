04b3b2cc60b79607b3873ebc4408fb92b7835cba
arthur <arthur.rios007@gmail.com>
2026-04-13 17:00:05 -0300

    Testes Unitários - Agrupando testes com Describe

--- src/utils/formatTime.test.ts
+++ src/utils/formatTime.test.ts
@@ -1,13 +1,20 @@
 import { formatPrice } from "."
 
-// test("formatPrice should format a price to 2 decimal places", () => {
-//   // executa o teste
-//   const formattedPrice = formatPrice(1000)
-//   expect(formattedPrice).toBe("1,000.00")
-// })
-
-it("should format a price to 2 decimal places", () => {
-  // executa o teste
-  const formattedPrice = formatPrice(1000)
-  expect(formattedPrice).toBe("1,000.00")
+describe("formatPrice", () => {
+  it("should format a price to 2 decimal places", () => {
+    const formattedPrice = formatPrice(1000)
+    expect(formattedPrice).toBe("1,000.00")
+  })
+  it("should round correctly", () => {
+    const formattedPrice = formatPrice(1000.121)
+    expect(formattedPrice).toBe("1,000.12")
+  })
+  it("should handle 0 correctly", () => {
+    const formattedPrice = formatPrice(0)
+    expect(formattedPrice).toBe("0.00")
+  })
+  it("should handle negative prices correctly", () => {
+    const formattedPrice = formatPrice(-1000)
+    expect(formattedPrice).toBe("-1,000.00")
+  })
 })
