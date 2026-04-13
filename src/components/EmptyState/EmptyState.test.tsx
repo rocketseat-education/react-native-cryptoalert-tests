8d44e2d5cd6fa5e4c3315171ac16399f8b631cc6
arthur <arthur.rios007@gmail.com>
2026-04-13 17:29:00 -0300

    Testes de componentes - Criando nossos primeiros testes

---
+++ src/components/EmptyState/EmptyState.test.tsx
@@ -0,0 +1,26 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { Text } from "react-native";
+import { EmptyState } from "./EmptyState";
+
+describe("EmptyState", () => {
+  it("should render the title and description", () => {
+    const title = "Nenhum alert encontrado"
+    const description = "Crie seu primeiro alerta para ser notificado quando uma criptomoeda atingir seu preço alvo."
+    const icon = "🚀"
+    const { getByText } = render(<EmptyState icon={<Text>{icon}</Text>} title={title} description={description} />)
+
+    expect(getByText(icon)).toBeTruthy()
+    expect(getByText(title)).toBeTruthy()
+    expect(getByText(description)).toBeTruthy()
+  })
+  it("should render the action button", () => {
+    const title = "Nenhum alert encontrado"
+    const description = "Crie seu primeiro alerta para ser notificado quando uma criptomoeda atingir seu preço alvo."
+    const icon = "🚀"
+    const action = <Text>Criar alerta</Text>
+    const { getByText } = render(<EmptyState icon={<Text>{icon}</Text>} title={title} description={description} action={<Text>{action}</Text>} />)
+
+    expect(getByText("Criar alerta")).toBeTruthy()
+  })
+})
