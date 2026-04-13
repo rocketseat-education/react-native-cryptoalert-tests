67ef340814cd3a3728057f974a4f42eba3130565
arthur <arthur.rios007@gmail.com>
2026-04-13 16:55:33 -0300

    Testes Unitários - Criando primeiro teste

--- src/utils/formatTime.test.ts
+++ src/utils/formatTime.test.ts
@@ -1,3 +1,7 @@
-test("test", () => {
+import { formatPrice } from "."
+
+test("formatPrice should format a price to 2 decimal places", () => {
   // executa o teste
+  const formattedPrice = formatPrice(1000)
+  expect(formattedPrice).toBe("1,000.00")
 })
