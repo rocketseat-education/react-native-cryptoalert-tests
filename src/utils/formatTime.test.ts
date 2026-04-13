222a37d021547924c65183df80093cb07adac014
arthur <arthur.rios007@gmail.com>
2026-04-13 16:56:43 -0300

    Testes Unitários - Uitlizando test : it

--- src/utils/formatTime.test.ts
+++ src/utils/formatTime.test.ts
@@ -1,6 +1,12 @@
 import { formatPrice } from "."
 
-test("formatPrice should format a price to 2 decimal places", () => {
+// test("formatPrice should format a price to 2 decimal places", () => {
+//   // executa o teste
+//   const formattedPrice = formatPrice(1000)
+//   expect(formattedPrice).toBe("1,000.00")
+// })
+
+it("should format a price to 2 decimal places", () => {
   // executa o teste
   const formattedPrice = formatPrice(1000)
   expect(formattedPrice).toBe("1,000.00")
