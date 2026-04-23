4b61753a3e3e1cb865fbb50b3e62d0b6e640fd11
arthur <arthur.rios007@gmail.com>
2026-04-23 15:42:38 -0300

    Mocks - Mock de API pt01 - Entendendo nosso arquivo de API

--- src/data/cryptoData.ts
+++ src/data/cryptoData.ts
@@ -15,6 +15,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "BTC",
     price: 67999.5,
     change24h: 2.45,
+    image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1747033579",
   },
   {
     id: "ethereum",
@@ -22,6 +23,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "ETH",
     price: 3850.25,
     change24h: -1.23,
+    image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1747033538",
   },
   {
     id: "cardano",
@@ -29,6 +31,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "ADA",
     price: 0.58,
     change24h: 5.67,
+    image: "https://assets.coingecko.com/coins/images/975/large/cardano.png?1747033579",
   },
   {
     id: "solana",
@@ -36,6 +39,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "SOL",
     price: 145.32,
     change24h: 3.89,
+    image: "https://assets.coingecko.com/coins/images/4128/large/solana.png?1747033571",
   },
   {
     id: "ripple",
@@ -43,6 +47,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "XRP",
     price: 0.52,
     change24h: -0.45,
+    image: "https://assets.coingecko.com/coins/images/4128/large/solana.png?1747033571",
   },
   {
     id: "polkadot",
@@ -50,6 +55,7 @@ export const cryptocurrencies: Cryptocurrency[] = [
     symbol: "DOT",
     price: 7.23,
     change24h: 1.98,
+    image: "https://assets.coingecko.com/coins/images/4128/large/solana.png?1747033571",
   },
   {
     id: "dogecoin",
