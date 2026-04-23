ee99621b5f90443b7f5c566fb9bdee289e22a6d0
arthur <arthur.rios007@gmail.com>
2026-04-23 17:45:10 -0300

    Mocks - Testes do CryptoCard pt01 - (price, symbol, name, avatar)

--- src/components/CryptoCard/CryptoCard.test.tsx
+++ src/components/CryptoCard/CryptoCard.test.tsx
@@ -1,15 +1,31 @@
 import { AlertProvider } from "@context/AlertProvider/AlertProvider";
-import { render } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 import { cryptoCurrenciesMock } from "../../../__mocks__/data/cryptoCurrencies";
+import { formatPrice } from "../../utils";
 import { CryptoCard } from "./CryptoCard";
 describe("Component: CryptoCard", () => {
-  it("should render the component", () => {
-    const {getByText} = render(
+  it("should render the crypto name, symbol and price", () => {
+    const { getByText, debug } = render(
       <AlertProvider>
         <CryptoCard crypto={cryptoCurrenciesMock[0]} />
       </AlertProvider>
     )
-    getByText(cryptoCurrenciesMock[0].name)
-    expect(getByText(cryptoCurrenciesMock[0].name)).toBeTruthy()
+    debug()
+    const name =getByText(cryptoCurrenciesMock[0].name)
+    const symbol = getByText(cryptoCurrenciesMock[0].symbol)
+    const price = getByText(`$${formatPrice(cryptoCurrenciesMock[0].price)}`)
+    expect(name).toBeTruthy()
+    expect(symbol).toBeTruthy()
+    expect(price).toBeTruthy()
+  })
+
+  it("should render first letter of the crypto symbol as avatar if crypto has no image", () => {
+    render(
+      <AlertProvider>
+        <CryptoCard crypto={{ ...cryptoCurrenciesMock[0], image: undefined }} />
+      </AlertProvider>
+    )
+    const avatar = screen.getByText(cryptoCurrenciesMock[0].symbol.charAt(0))
+    expect(avatar).toBeTruthy()
   })
 })
