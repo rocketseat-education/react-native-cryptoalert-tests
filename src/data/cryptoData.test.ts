b1339300f1db65a6b72fe3f7c0f3a876d90435f9
arthur <arthur.rios007@gmail.com>
2026-04-23 16:03:39 -0300

    Mocks - Mock de API pt04 - Implementando múltiplos expects

--- src/data/cryptoData.test.ts
+++ src/data/cryptoData.test.ts
@@ -1,13 +1,16 @@
-import { cryptocurrencies, fetchCryptocurrenciesFromApi } from "./cryptoData"
+import { cryptocurrenciesMockResponse, fetchCryptocurrenciesFromApi } from "./cryptoData"
 
 describe("API: CryptoData", () => {
   it("should fetch cryptocurrencies from the API", async () => {
     jest.spyOn(globalThis, "fetch").mockResolvedValue({
       ok: true,
-      json: async () => cryptocurrencies
+      json: async () => cryptocurrenciesMockResponse
     } as Response)
 
     const data = await fetchCryptocurrenciesFromApi()
     console.log(data)
+    expect(data).toBeDefined()
+    expect(Array.isArray(data)).toBe(true)
+    expect(data.length).toBe(cryptocurrenciesMockResponse.length)
   })
 })
