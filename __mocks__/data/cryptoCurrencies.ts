4c4031701955388c963bbfc9159bf33f3c1dc328
arthur <arthur.rios007@gmail.com>
2026-04-23 17:36:40 -0300

    Mocks - Importância do Mock do Módulo Nativo - AsyncStorage

---
+++ __mocks__/data/cryptoCurrencies.ts
@@ -0,0 +1,26 @@
+export const cryptoCurrenciesMock = [
+  {
+    id: "bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    price: 67999.5,
+    change24h: 2.45,
+    image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1747033579",
+  },
+  {
+    id: "ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    price: 3850.25,
+    change24h: -1.23,
+    image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1747033538",
+  },
+  {
+    id: "cardano",
+    name: "Cardano",
+    symbol: "ADA",
+    price: 0.58,
+    change24h: 5.67,
+    image: "https://assets.coingecko.com/coins/images/975/large/cardano.png?1747033579",
+  },
+]
