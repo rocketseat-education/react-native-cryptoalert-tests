17b294097a3880bed9e18116d4adfcb0ec6bcc10
arthur <arthur.rios007@gmail.com>
2026-05-05 12:27:07 -0300

    Testando hooks - Removendo alertas com act

--- src/context/AlertProvider/useAlertProvider.test.ts
+++ src/context/AlertProvider/useAlertProvider.test.ts
@@ -1,7 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { act, renderHook, waitFor } from "@testing-library/react-native";
 import { useAlertProviderState } from "./useAlertProvider";
 
 describe("Context: AlertProvider", () => {
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    await AsyncStorage.clear();
+  });
+
   it("should add an alert", async () => {
     const { result } = renderHook(() => useAlertProviderState())
     
@@ -18,5 +24,27 @@ describe("Context: AlertProvider", () => {
       });
     });
     expect(result.current.alerts).toHaveLength(1);
+  });
+  it("should delete an alert", async () => {
+    const { result } = renderHook(() => useAlertProviderState())
+    
+    await waitFor(() => {
+      expect(result.current.alerts).toHaveLength(0);
+    });
+
+    act(() => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+    expect(result.current.alerts).toHaveLength(1);
+    const alertToDelete = result.current.alerts[0];
+    act(() => {
+      result.current.deleteAlert(alertToDelete.id);
+    });
+    expect(result.current.alerts).toHaveLength(0);
   });
 });
