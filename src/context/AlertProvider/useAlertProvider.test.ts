98c47f38aa70d42aef2320f92ccafd054f9f3d91
arthur <arthur.rios007@gmail.com>
2026-05-05 12:33:14 -0300

    Testando hooks - Testando a persistência do hook

--- src/context/AlertProvider/useAlertProvider.test.ts
+++ src/context/AlertProvider/useAlertProvider.test.ts
@@ -1,12 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { act, renderHook, waitFor } from "@testing-library/react-native";
+import { cryptoAlertsMock } from "../../../__mocks__/storage/cryptoAlerts";
 import { useAlertProviderState } from "./useAlertProvider";
 
 describe("Context: AlertProvider", () => {
-  beforeEach(async () => {
-    jest.clearAllMocks();
-    await AsyncStorage.clear();
-  });
 
   it("should add an alert", async () => {
     const { result } = renderHook(() => useAlertProviderState())
@@ -24,6 +21,7 @@ describe("Context: AlertProvider", () => {
       });
     });
     expect(result.current.alerts).toHaveLength(1);
+    AsyncStorage.clear();
   });
   it("should delete an alert", async () => {
     const { result } = renderHook(() => useAlertProviderState())
@@ -46,5 +44,32 @@ describe("Context: AlertProvider", () => {
       result.current.deleteAlert(alertToDelete.id);
     });
     expect(result.current.alerts).toHaveLength(0);
+  });
+
+  it('should load alerts from AsyncStorage', async () => {
+    jest.spyOn(AsyncStorage, 'getItem').mockResolvedValue(JSON.stringify(cryptoAlertsMock));
+    const { result } = renderHook(() => useAlertProviderState())
+    await waitFor(() => {
+      expect(result.current.alerts).toHaveLength(2);
+    });
+  });
+
+  it('should save alerts to AsyncStorage', async () => {
+    const { result } = renderHook(() => useAlertProviderState())
+    
+    await waitFor(() => {
+      expect(result.current.alerts).toHaveLength(0);
+    });
+
+    act(() => {
+      result.current.addAlert({
+        cryptocurrency: "Bitcoin",
+        symbol: "BTC",
+        targetPrice: 100000,
+        condition: "above",
+      });
+    });
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith("cryptoAlerts", JSON.stringify(result.current.alerts));
+    AsyncStorage.clear();
   });
 });
