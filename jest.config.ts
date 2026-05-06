694dfacf68a32cb1122f5a95b0adb7efd6c66d8a
arthur <arthur.rios007@gmail.com>
2026-05-06 08:08:00 -0300

    Cobertura de Código - Configurações do Coverage Report pt03

--- jest.config.ts
+++ jest.config.ts
@@ -30,7 +30,21 @@ const config: Config = {
     "json",
     "lcov",
     "html"
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+    "./src/services/": {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    }
+  }
 }
 
 export default config
