ce705f2efbfa6bb2018ae2370936b5f3e4bf6285
arthur <arthur.rios007@gmail.com>
2026-05-06 08:01:05 -0300

    Cobertura de Código - Configurações do Coverage Report pt01

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,21 @@ import type { Config } from "jest"
 const config: Config = {
   preset: "jest-expo",
   setupFiles: ["<rootDir>/jest.setup.js"],
+  coverageDirectory: "./__tests__/coverage",
   transformIgnorePatterns: [
     "node_modules/(?!(\\.pnpm|(jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@react-native-async-storage/)"
   ],
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/__tests__/utils/"
+    "/__tests__/utils/",
+    "/__tests__/coverage/"
+  ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    '!src/**/*.test.tsx',
+    '!src/**/*.test.ts',
+    '!src/**/*.styles.ts',
   ]
 }
 
